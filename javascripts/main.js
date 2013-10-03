b9002bf0125efa4d2e9dca17526c452530133064
Michele Gobbi <info@dynamick.it>
2013-10-03 22:09:38 +0200

    demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+(function() {
+
+  // initialize Masonry
+  var $container = $('#container');
+
+  $container.filteredMasonry({
+    itemSelector: '.challenge',
+    filtersGroupSelector:'.filters'
+  });
+
+});
